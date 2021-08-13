0e99927b44848cccde126a288fb8e3378f5e31ec
Abhishek Anand <anand@cloudcover.in>
2021-08-13 16:26:56 +0530

    updating script

--- library/general/validate.sh
+++ library/general/validate.sh
@@ -3,12 +3,11 @@ input=$1
 while IFS= read -r line
 do
   echo  "$line"
-  kubectl kustomize $line | kubectl apply -f - --validate=false
   cd "$line"
   ls
   cd samples/*
   ls -lrt
-  kubectl apply -f example_disallowed.yaml
+  kubectl apply -f example_disallowed.yaml -n dev
   cd ../../../
   pwd
 done < "$input"
